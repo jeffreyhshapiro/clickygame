07d9bea1e393056d77ad761561e201877ba7595c
unknown <jeffreyhshapiro@gmail.com>
2015-11-15 20:49:15 -0500

    Inital commit for clickygame. To be updated with more JS

---
+++ face_counter.js
@@ -0,0 +1,32 @@
+var counter = 0;
+function countup() {
+  counter = counter + 1;
+  }
+
+
+function countdown() {
+  setTimeout(function countandgive()
+    { 
+    alert("You got " +counter+ " characters!");
+     
+     }, 5000);
+}
+
+
+
+
+
+
+
+
+
+
+
+//var imageElements = document.getElementsByTagName('img');
+
+//function timerInterval() {
+
+//}
+
+
+//document.getElementById('start-button').addEventListener("click", timerInterval);*/
